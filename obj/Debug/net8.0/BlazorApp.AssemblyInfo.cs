21c626dc08d65a8b1c9bb411a4efe37486e1b806
MuGyeongJeong <jmg3897@icloud.com>
2024-05-28 17:21:51 +0900

    코드수정

--- obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
+++ obj/Debug/net8.0/BlazorApp.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+b673a8539c344151eb18e9dfa689988912e2f5eb")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+576560f46580e62533f4f22305c153eaaeb6e553")]
 [assembly: System.Reflection.AssemblyProductAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyTitleAttribute("BlazorApp")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
